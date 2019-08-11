213813f86336fcecf3dd03a1c249e41d6d87c146
harell <harel@loostiger.com>
2019-08-11 16:21:33 +1200

    convert var types

--- R/PrepareData.R
+++ R/PrepareData.R
@@ -39,6 +39,38 @@ PrepareData <- R6::R6Class(
 # Private Methods: High-level Functions ----------------------------------------
 .cast_data <- function(private){
     message("-> Casting Data")
+    starts_with_has <- function(.data) .data %>% colnames() %>% stringr::str_detect("^has_") %>% which()
+
+    table <- dplyr::bind_rows(train = private$.historical_data, test = private$.new_data, .id = "Source")
+
+    table <-
+        table %>%
+        dplyr::mutate(
+            building_id = as.character(building_id),
+            geo_level_1_id = as.integer(geo_level_1_id),
+            geo_level_2_id = as.integer(geo_level_2_id),
+            geo_level_3_id = as.integer(geo_level_3_id),
+            count_floors_pre_eq = as.integer(count_floors_pre_eq),
+            age = as.integer(age),
+            area_percentage = as.integer(area_percentage),
+            height_percentage = as.integer(height_percentage),
+            land_surface_condition = as.factor(land_surface_condition),
+            foundation_type = as.factor(foundation_type),
+            roof_type = as.factor(roof_type),
+            ground_floor_type = as.factor(ground_floor_type),
+            other_floor_type = as.factor(other_floor_type),
+            position = as.factor(position),
+            plan_configuration = as.factor(plan_configuration),
+            legal_ownership_status = as.factor(legal_ownership_status),
+            count_families = as.integer(count_families)
+        ) %>%
+        dplyr::mutate_at(starts_with_has(table), as.logical) %>%
+        as.data.frame()
+
+    private$.historical_data <- table %>% dplyr::filter(Source == "train") %>% dplyr::select(-Source)
+    private$.new_data <- table %>% dplyr::filter(Source == "test") %>% dplyr::select(-Source)
+
+    return(invisible())
     invisible(private)
 }
 
