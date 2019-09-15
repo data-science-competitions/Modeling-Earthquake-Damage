cfc66b924a2e2da5b759e841798ce688950a6cc8
harell <harel@loostiger.com>
2019-09-15 20:06:16 +1200

    add_step(step_install_development_tools())

--- tic.R
+++ tic.R
@@ -3,7 +3,7 @@ invisible(sapply(list.files("./.tic", full.names = TRUE), source))
 
 # Stage: Before Install --------------------------------------------------------
 get_stage("before_install") %>%
-    # add_step(step_install_development_tools()) %>%
+    add_step(step_install_development_tools()) %>%
     add_step(step_install_deps())
 
 # Stage: Install
