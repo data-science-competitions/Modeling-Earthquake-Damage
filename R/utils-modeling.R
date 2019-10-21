05ab460c471eac2bfeae5c115ee827f00917fe47
harell <harel@loostiger.com>
2019-10-22 09:47:46 +1300

    compare mpg to itself

--- R/utils-modeling.R
+++ R/utils-modeling.R
@@ -1,4 +1,40 @@
 # evaluate_model ----------------------------------------------------------
 #
+#' @title  Estimate How Well a Model Works
+#'
+#' @description Calculate the model's \code{"rmse", "mae", "iic"}.
+#'
+#' @param data (`data.frame`) A table containing the \code{truth} and\code{estimate} columns.
+#' @param truth (`character`) The column identifier for the true results.
+#' @param estimate (`character`) The column identifier for the predictedresults.
+#'
+#' @return (`data.frame`) A table with columns .metric, .estimator, and
+#'   .estimate and 1 row of values. For grouped data frames, the number of rows
+#'   returned will be the same as the number of groups.
+#'
+#' @family modeling utility functions
+#' @export
+#'
+evaluate_model <- function(data, truth, estimate){
+    stopifnot(all(c(truth, estimate) %in% colnames(data)))
 
+    # Regression Metrics
+    data[, truth] <- data[, truth] %>% as.character() %>% as.numeric()
+    regression_metrics <- tibble::tibble()
+    criteria <- c("rmse", "mae")
+    for(criterion in criteria){
+        command <- paste0("yardstick::", criterion, "(data, !!truth, !!estimate)")
+        regression_metric <- try(eval(expr = parse(text = command)))
+        if("try-error" %in% class(regression_metric)) next()
+        regression_metrics <- dplyr::bind_rows(regression_metrics, regression_metric)
+    }
+
+    # Classification Metrics
+    data[, truth] <- data[, truth] %>% as.factor()
+    classification_metrics <- tibble::tibble()
+
+    # Return
+    metrics <- dplyr::bind_rows(regression_metrics, classification_metrics)
+    return(metrics)
+}
 
--- tests/testthat/test-utils-modeling.R
+++ tests/testthat/test-utils-modeling.R
@@ -1,7 +1,19 @@
 context("unit test utils-modeling")
 
+testthat::setup({
+    assign("test_env", testthat::test_env(), envir = parent.frame())
+
+    set.seed(921)
+    mpg_hat <- mtcars$mpg + rnorm(nrow(mtcars))
+    mpg_copy <- mtcars$mpg
+    test_env$dataset <- cbind(mpg_hat, mpg_copy, mtcars)
+})
+
 # evaluate_model ----------------------------------------------------------
 test_that("evaluate_model works", {
-
+    attach(test_env)
+    expect_silent(results <- evaluate_model(data = dataset, truth = "mpg", estimate = "mpg_copy"))
+    expect_class(results, "data.frame")
+    expect_table_has_col_names(results, c(".metric", ".estimator", ".estimate"))
 })
 
