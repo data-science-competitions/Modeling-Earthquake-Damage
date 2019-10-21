ae7c146daf8cfdf23fc08abfe64ce155cb7ba447
harell <harel@loostiger.com>
2019-10-21 13:51:22 +1300

    added link function

--- inst/models/rpart-tree/model_predict.R
+++ inst/models/rpart-tree/model_predict.R
@@ -4,11 +4,21 @@
 #' @return A vector of predictions
 model_predict <- function(new_data, model_object)
 {
-    response <- predict(
-        object = model_object,
-        newdata = new_data,
-        type = c("vector", "prob", "class", "matrix")[1]
-    )
+    predict_function <- function(new_data, model_object){
+        response <- predict(
+            object = model_object,
+            newdata = new_data,
+            type = c("vector", "prob", "class", "matrix")[1]
+        )
+    }
 
-    return(response)
+    link_function <- function(x){ # 1 <= x <= 3
+        minmax <- function(x, a, b) pmin(pmax(x, a), b)
+        normalize <- function(x) (x - min(x)) / (max(x) - min(x))
+
+        y <- x %>% minmax(1, 3) %>% scale() %>% normalize()
+        y <- y * 2 + 1
+    }
+
+    new_data %>% predict_function(model_object) %>% link_function()
 }
