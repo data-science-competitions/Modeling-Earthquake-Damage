c63ad25acbf1aed6344a97afcbb7c2665f8060e9
harell <harel@loostiger.com>
2019-10-23 09:26:38 +1300

    unit test covers unknown metrics

--- tests/testthat/test-Yardstick.R
+++ tests/testthat/test-Yardstick.R
@@ -13,7 +13,8 @@ test_that("Yardstick constructor works", {
     expect_class(Yardstick$new(data = data, truth = "mpg", estimate = "mpg_hat"), "Yardstick")
 })
 
-# metrics -----------------------------------------------------------------
+
+# known metrics -----------------------------------------------------------
 test_that("Yardstick metrics are available", {
     attach(test_env)
 
@@ -21,3 +22,13 @@ test_that("Yardstick metrics are available", {
     for(metric in c("rmse", "mae", "rsq", "ccc"))
         expect_a_non_empty_data.frame(metrics[[metric]])
 })
+
+# unknown metrics ---------------------------------------------------------
+test_that("Yardstick unknown metrics are return NULL", {
+    attach(test_env)
+
+    expect_silent(metrics <- Yardstick$new(data = data, truth = "mpg", estimate = "mpg_hat"))
+    expect_null(metrics[["unknown_metric"]])
+})
+
+
