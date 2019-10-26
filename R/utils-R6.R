3d2348497e3af403440f4f6d4dd8a42d644bcdaa
harell <harel@loostiger.com>
2019-10-26 14:15:13 +1300

    .delete_shared_env

--- R/utils-R6.R
+++ R/utils-R6.R
@@ -42,3 +42,16 @@ utils::globalVariables("shared_env")
     .create_shared_env(key, value)
     invisible()
 }
+
+#' @title Delete an Object Within Shared Evnironment
+#' @inheritParams .create_shared_env
+#' @inheritSection .create_shared_env CRUD
+#' @family Shared Environment CRUD API
+#' @keywords internal
+#' @export
+#' @noRd
+.delete_shared_env <- function(key){
+    stopifnot(exists("shared_env"), is.environment(shared_env))
+    rm(list = key, envir = shared_env)
+    invisible()
+}
--- tests/testthat/test-utils-R6.R
+++ tests/testthat/test-utils-R6.R
@@ -4,6 +4,7 @@ context("unit test for utils-R6")
 testthat::setup({
     assign("test_env", testthat::test_env(), envir = parent.frame())
     test_env$shared_env <- new.env()
+    test_env$key_without_value <- "vegetable"
     test_env$key <- "fruit"
     test_env$value <- "Apple"
     test_env$new_value <- "Papaya"
@@ -22,6 +23,13 @@ test_that("CRUD updates a new object", {
     expect_null(.create_shared_env(key, value))
     expect_null(.update_shared_env(key, new_value))
     expect_identical(.read_shared_env(key), new_value)
+})
+
+test_that("CRUD deletes an existing object", {
+    attach(test_env)
+
+    expect_null(.create_shared_env(key, value))
+    expect_null(.delete_shared_env(key))
 })
 
 # reading non existing object
