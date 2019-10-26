34c0e4866c58a417d660b18169123475f6328265
harell <harel@loostiger.com>
2019-10-26 13:55:24 +1300

    .create_shared_env and .read_shared_env

--- R/utils-R6.R
+++ R/utils-R6.R
@@ -1,2 +1,31 @@
 # Shared Environment CRUD API ---------------------------------------------
 utils::globalVariables("shared_env")
+
+#' @title Create an Object in Shared Evnironment
+#' @param key (`character`) The name of the object.
+#' @param value (`?`) The object.
+#' @section CRUD: \url{https://en.wikipedia.org/wiki/Create,_read,_update_and_delete}
+#' @family Shared Environment CRUD API
+#' @keywords internal
+#' @export
+#' @noRd
+.create_shared_env <- function(key, value){
+    stopifnot(is.character(key), length(key) == 1)
+    stopifnot(exists("shared_env"), is.environment(shared_env))
+    assign(key, value, envir = shared_env)
+    invisible()
+}
+
+#' @title Read an Object from Shared Evnironment
+#' @inheritParams .create_shared_env
+#' @return The queried object.
+#' @inheritSection .create_shared_env CRUD
+#' @family Shared Environment CRUD API
+#' @keywords internal
+#' @export
+#' @noRd
+.read_shared_env <- function(key){
+    stopifnot(is.character(key), length(key) == 1)
+    stopifnot(exists("shared_env"), is.environment(shared_env))
+    get(key, envir = shared_env)
+}
--- tests/testthat/test-utils-R6.R
+++ tests/testthat/test-utils-R6.R
@@ -3,17 +3,17 @@ context("unit test for utils-R6")
 # Setup -------------------------------------------------------------------
 testthat::setup({
     assign("test_env", testthat::test_env(), envir = parent.frame())
-
-    set.seed(921)
-    mpg_hat <- mtcars$mpg + rnorm(nrow(mtcars))
-    mpg_copy <- mtcars$mpg
-    test_env$dataset <- cbind(mpg_hat, mpg_copy, mtcars)
+    test_env$shared_env <- new.env()
+    test_env$key <- "fruit"
+    test_env$value <- "apple"
 })
 
 # Shared Environment CRUD API ---------------------------------------------
-test_that("CRUD API: .create_shared_env works", {
+test_that("CRUD API simple case", {
     attach(test_env)
 
+    expect_null(.create_shared_env(key, value))
+    expect_identical(.read_shared_env(key), value)
 })
 
 
