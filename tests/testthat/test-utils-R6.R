b9051d4547ee4288680c7ff2e803e2c9690cb59e
harell <harel@loostiger.com>
2019-10-26 14:06:37 +1300

    split test cases

--- tests/testthat/test-utils-R6.R
+++ tests/testthat/test-utils-R6.R
@@ -10,12 +10,18 @@ testthat::setup({
 })
 
 # Shared Environment CRUD API ---------------------------------------------
-test_that("CRUD API simple case", {
+test_that("CRUD creates a new object", {
+    attach(test_env)
+    expect_null(.create_shared_env(key, value))
+    expect_identical(.read_shared_env(key), value)
+})
+
+test_that("CRUD updates a new object", {
     attach(test_env)
 
     expect_null(.create_shared_env(key, value))
-    expect_identical(.read_shared_env(key), value)
     expect_null(.update_shared_env(key, new_value))
+    expect_identical(.read_shared_env(key), new_value)
 })
 
 # reading non existing object
