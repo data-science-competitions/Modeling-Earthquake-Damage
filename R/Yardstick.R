20f919b999b4c7e1eeb0d37132b413e1669d94e4
harell <harel@harmonic.co.nz>
2019-11-01 14:19:27 +1300

    added gain plot and lift plot

--- R/Yardstick.R
+++ R/Yardstick.R
@@ -12,8 +12,13 @@
 #' * \code{estimate} (`character`) The column identifier for the predicted results.
 #'
 #' @section Public Methods:
+#' * \code{set_threshold} TRUE if x > threshold; FALSE if x <= threshold.
 #' * \code{insert_label}
 #' * \code{delete_label}
+#' * \code{plot_gain_curve} A cumulative gains curve shows the total number of
+#' events captured by a model over a given number of samples.
+#' * \code{plot_lift_curve} A lift curve shows the ratio of a model to a random
+#' guess ('model cumulative sum' / 'random guess').
 #'
 #' @return (`Yardstick`) A Yardstick object.
 #'
@@ -28,6 +33,7 @@
 #' metrics$rmse
 #' }
 #'
+#' @import ggplot2
 #' @import yardstick
 #' @export
 #'
@@ -45,11 +51,15 @@ Yardstick <- R6::R6Class(
             private$.truth <- truth
             private$.estimate <- estimate
         },
+        set_threshold = function(value) .set_threshold(value, private),
         insert_label = function(key, value) .insert_label(key, value, private),
-        delete_label = function(key) .delete_label(key, private)
+        delete_label = function(key) .delete_label(key, private),
+        plot_gain_curve = function() .plot_gain_curve(private),
+        plot_lift_curve = function() .plot_lift_curve(private)
     ),
     private = list(
         ## Private Variables
+        .threshold = NULL,
         .dictionary = data.frame(key = c(".metric", ".estimator", ".estimate"), value = NA_character_, stringsAsFactors = FALSE),
         .data = data.frame(stringsAsFactors = FALSE),
         .truth = character(0),
@@ -67,6 +77,11 @@ Yardstick <- R6::R6Class(
 )
 
 # Public Methods ----------------------------------------------------------
+.set_threshold <- function(value, private){
+    private$.threshold <- value
+    invisible()
+}
+
 .insert_label <- function(key, value, private){
     new_entry <- data.frame(key = key, value = value, stringsAsFactors = FALSE)
     private$.dictionary <- rbind(new_entry, private$.dictionary) %>% dplyr::distinct(key, .keep_all = TRUE)
@@ -77,6 +92,37 @@ Yardstick <- R6::R6Class(
     dictionary <- private$.dictionary
     private$.dictionary <- dictionary[!dictionary$key %in% key, ]
     invisible()
+}
+
+.plot_gain_curve <- function(private){
+    .check_classification_plot_prerequisites(private)
+
+    ggplot_data <- .get_classification_plot_data(private)
+
+    ggplot_fig <-
+        ggplot_data %>%
+        yardstick::gain_curve(!!private$.truth, !!private$.estimate) %>%
+        autoplot() +
+        coord_fixed(ratio = 1) +
+        scale_x_continuous(breaks = seq(0, 100, by = 10), expand = c(0,0)) +
+        scale_y_continuous(breaks = seq(0, 100, by = 10), expand = c(0,0))
+
+    return(ggplot_fig)
+}
+
+.plot_lift_curve <- function(private){
+    .check_classification_plot_prerequisites(private)
+
+    ggplot_data <- .get_classification_plot_data(private)
+
+    ggplot_fig <-
+        ggplot_data %>%
+        yardstick::lift_curve(!!private$.truth, !!private$.estimate) %>%
+        autoplot() +
+        scale_x_continuous(breaks = seq(0, 100, by = 10)) +
+        scale_y_continuous(breaks = seq(1, 100, by = 0.5))
+
+    return(ggplot_fig)
 }
 
 # Private Methods ---------------------------------------------------------
@@ -102,3 +148,32 @@ Yardstick <- R6::R6Class(
     return(results)
 }
 
+# Public Methods Helper Functions -----------------------------------------
+.check_classification_plot_prerequisites <- function(private){
+    data <- private$.data
+    truth <- private$.truth
+    estimate <- private$.estimate
+    threshold <- private$.threshold
+
+    if(data[[truth]] %>% is.numeric()){
+        if(is.null(threshold)) stop("No threshold value is defined; use set_threshold() to define it.")
+        if(max(data[[truth]]) > 1 | min(data[[truth]]) < 0) stop("`truth` has values outside the bounded interval [0,1]")
+    }
+
+    if(data[[estimate]] %>% is.numeric()){
+        if(max(data[[estimate]]) > 1 | min(data[[estimate]]) < 0) stop("`estimate` has values outside the bounded interval [0,1]")
+    }
+
+    invisible()
+}
+
+.get_classification_plot_data <- function(private){
+    data <- private$.data
+    truth <- private$.truth
+    estimate <- private$.estimate
+    threshold <- private$.threshold
+
+    ggplot_data <- dplyr::mutate(data, !!truth := factor(truth > threshold) %>% relevel(ref = "TRUE"))
+
+    return(ggplot_data)
+}
--- tests/testthat/test-Yardstick.R
+++ tests/testthat/test-Yardstick.R
@@ -3,21 +3,27 @@ context("unit test for Yardstick")
 # Setup -------------------------------------------------------------------
 testthat::setup({
     assign("test_env", testthat::test_env(), envir = parent.frame())
+    set.seed(1001)
+
     mpg_hat <- mtcars$mpg
-    test_env$data <- cbind(mpg_hat, mtcars)
+    test_env$data_reg <- cbind(mpg_hat, mtcars)
+
+    setosa.truth <- as.numeric(iris$Species == "setosa")
+    setosa.estimate <- runif(length(setosa.truth))
+    test_env$data_cla <- cbind(iris, setosa.truth, setosa.estimate)
 })
 
 # constructor -------------------------------------------------------------
 test_that("Yardstick constructor works", {
     attach(test_env)
-    expect_class(Yardstick$new(data = data, truth = "mpg", estimate = "mpg_hat"), "Yardstick")
+    expect_class(Yardstick$new(data = data_reg, truth = "mpg", estimate = "mpg_hat"), "Yardstick")
 })
 
 # known metrics -----------------------------------------------------------
 test_that("Yardstick metrics are available", {
     attach(test_env)
 
-    expect_silent(metrics <- Yardstick$new(data = data, truth = "mpg", estimate = "mpg_hat"))
+    expect_silent(metrics <- Yardstick$new(data = data_reg, truth = "mpg", estimate = "mpg_hat"))
     for(metric in c("rmse", "mae", "rsq", "ccc"))
         expect_a_non_empty_data.frame(metrics[[metric]])
 })
@@ -26,7 +32,7 @@ test_that("Yardstick metrics are available", {
 test_that("Yardstick unknown metrics are return NULL", {
     attach(test_env)
 
-    expect_silent(metrics <- Yardstick$new(data = data, truth = "mpg", estimate = "mpg_hat"))
+    expect_silent(metrics <- Yardstick$new(data = data_reg, truth = "mpg", estimate = "mpg_hat"))
     expect_null(metrics[["unknown_metric"]])
 })
 
@@ -34,14 +40,14 @@ test_that("Yardstick unknown metrics are return NULL", {
 test_that("Yardstick keys are available", {
     attach(test_env)
 
-    expect_silent(metrics <- Yardstick$new(data = data, truth = "mpg", estimate = "mpg_hat"))
+    expect_silent(metrics <- Yardstick$new(data = data_reg, truth = "mpg", estimate = "mpg_hat"))
     expect_identical(metrics$keys, c(".metric", ".estimator", ".estimate"))
 })
 
 test_that("Yardstick allows to add attributes", {
     attach(test_env)
 
-    expect_silent(metrics <- Yardstick$new(data = data, truth = "mpg", estimate = "mpg_hat"))
+    expect_silent(metrics <- Yardstick$new(data = data_reg, truth = "mpg", estimate = "mpg_hat"))
     # Add ".set" for 1st time
     expect_silent(metrics$insert_label(key = ".set", value = "train"))
     expect_identical(metrics$keys, c(".set", ".metric", ".estimator", ".estimate"))
@@ -53,7 +59,7 @@ test_that("Yardstick allows to add attributes", {
 test_that("Yardstick adds attributes to results", {
     attach(test_env)
 
-    expect_silent(metrics <- Yardstick$new(data = data, truth = "mpg", estimate = "mpg_hat"))
+    expect_silent(metrics <- Yardstick$new(data = data_reg, truth = "mpg", estimate = "mpg_hat"))
     expect_silent(metrics$insert_label(key = ".set", value = "train"))
     expect_a_non_empty_data.frame(results <- metrics[["rmse"]])
     expect_table_has_col_names(results, c(".set", ".metric", ".estimator", ".estimate"))
@@ -63,7 +69,7 @@ test_that("Yardstick adds attributes to results", {
 test_that("Yardstick allows to remove attributes", {
     attach(test_env)
 
-    expect_silent(metrics <- Yardstick$new(data = data, truth = "mpg", estimate = "mpg_hat"))
+    expect_silent(metrics <- Yardstick$new(data = data_reg, truth = "mpg", estimate = "mpg_hat"))
     expect_silent(metrics$delete_label(key = ".metric"))
     expect_identical(metrics$keys, c(".estimator", ".estimate"))
 })
@@ -71,8 +77,59 @@ test_that("Yardstick allows to remove attributes", {
 test_that("Yardstick discards attributes from results", {
     attach(test_env)
 
-    expect_silent(metrics <- Yardstick$new(data = data, truth = "mpg", estimate = "mpg_hat"))
+    expect_silent(metrics <- Yardstick$new(data = data_reg, truth = "mpg", estimate = "mpg_hat"))
     expect_silent(metrics$delete_label(key = ".metric"))
     expect_a_non_empty_data.frame(results <- metrics[["rmse"]])
     expect_identical(colnames(results), c(".estimator", ".estimate"))
 })
+
+# set threshold -----------------------------------------------------------
+test_that("Yardstick allows to set a threshold value", {
+    attach(test_env)
+
+    expect_silent(metrics <- Yardstick$new(data = data_reg, truth = "mpg", estimate = "mpg_hat"))
+    expect_silent(metrics$set_threshold(value = 0.5))
+})
+
+# plot lift curve ---------------------------------------------------------
+test_that("Yardstick plots lift curve when truth is numeric", {
+    attach(test_env)
+
+    expect_silent(metrics <- Yardstick$new(data = data_cla, truth = "setosa.truth", estimate = "setosa.estimate"))
+    expect_silent(metrics$set_threshold(value = 0.5))
+    expect_class(metrics$plot_lift_curve(), "ggplot")
+
+    data <- data_cla %>% dplyr::rename("truth" = "setosa.truth", "estimate" = "setosa.estimate")
+    expect_silent(metrics <- Yardstick$new(data = data, truth = "truth", estimate = "estimate"))
+    expect_silent(metrics$set_threshold(value = 0.5))
+    expect_class(metrics$plot_lift_curve(), "ggplot")
+})
+
+test_that("Yardstick fails to plot lift curve when no threshold is set", {
+    attach(test_env)
+
+    expect_silent(metrics <- Yardstick$new(data = data_cla, truth = "setosa.truth", estimate = "setosa.estimate"))
+    expect_error(metrics$plot_lift_curve())
+})
+
+test_that("Yardstick fails to plot lift curve when truth or estimate are not within the [0,1] boundary", {
+    attach(test_env)
+
+    expect_silent(metrics <- Yardstick$new(data = data_cla, truth = "Sepal.Width", estimate = "setosa.estimate"))
+    expect_silent(metrics$set_threshold(value = 0.5))
+    expect_error(metrics$plot_lift_curve())
+
+    expect_silent(metrics <- Yardstick$new(data = data_cla, truth = "setosa.truth", estimate = "Sepal.Width"))
+    expect_silent(metrics$set_threshold(value = 0.5))
+    expect_error(metrics$plot_lift_curve())
+})
+
+# plot gain curve ---------------------------------------------------------
+test_that("Yardstick plots gain curve when truth is numeric", {
+    attach(test_env)
+
+    expect_silent(metrics <- Yardstick$new(data = data_cla, truth = "setosa.truth", estimate = "setosa.estimate"))
+    expect_silent(metrics$set_threshold(value = 0.5))
+    expect_class(metrics$plot_gain_curve(), "ggplot")
+})
+
