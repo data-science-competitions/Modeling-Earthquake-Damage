fff5387b013df1740385b1206ce13fdde814a834
harell <harel@loostiger.com>
2019-11-03 12:48:20 +1300

    install catboost version 0.18

--- inst/models/catboost/model_init.R
+++ inst/models/catboost/model_init.R
@@ -1,5 +1,17 @@
 #' @title Prepare everything the prediction model needs
 model_init <- function(){
+    is_package_not_installed <- function(pkg) !pkg %in% rownames(installed.packages())
+
+    if(is_package_not_installed("catboost")){
+        message("Installing catboost; this may take a few minutes")
+        os <- Sys.info()[['sysname']]
+        ver <- "0.18"
+        github_slug <- 'https://github.com/catboost/catboost/releases/download/'
+        url <- paste0(github_slug, "v", ver, "/catboost-R-", os, "-", ver, ".tgz")
+        remotes::install_url(url, args = c("--no-multiarch", "--no-test-load"))
+        # remotes::install_github('catboost/catboost', subdir = 'catboost/R-package')
+    }
+
     predict_function <- function(model_object, new_data){
         predict(object = model_object, data = new_data, type = "response") %>%
             ranger::predictions() %>%
