db6068ef018f26883faa6bfda9e50041ac43ec66
harell <harel@loostiger.com>
2019-11-09 16:54:34 +1300

    variable importance with catboost

--- inst/models/catboost/model_end.R
+++ inst/models/catboost/model_end.R
@@ -1,5 +1,26 @@
 #' @title Post Modelling Operations
 #' @description Execute code after completion of backtesting
 model_end <- function(){
+    ## Explore, Explain, and Debug model
+    target <- file.path(getOption("path_archive", default = tempdir()), model_name, "explanations")
+    dir.create(target, showWarnings = FALSE, recursive = TRUE)
+    explainer <- DALEX::explain(
+        model = model_object,
+        data = historical_data[, role_input],
+        y = historical_data[[role_target]],
+        predict_function = function(model, data){
+            data <- data %>% catboost::catboost.load_pool()
+            predict_function(model, data)[["fit"]]
+        },
+        link = link_function,
+        label = model_name,
+        verbose = FALSE
+    )
+
+    ## Visualisation: Variable Importance
+    figure_path <- file.path(target, "variable-importance.jpg")
+    explanation <- ingredients::feature_importance(explainer, type = "difference") %>% plot()
+    ggplot2::ggsave(figure_path, explanation, "jpeg", width = 297, height = 210, units = "mm")
+
     return(invisible())
 }
