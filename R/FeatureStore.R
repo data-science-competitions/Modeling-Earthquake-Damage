92c9b89ea75a47fd7883aad52ffdee27da0f24ae
harell <harel@loostiger.com>
2019-11-10 11:40:54 +1300

    geo_level_1_id_ordered and geo_level_1_id_integer

--- R/FeatureStore.R
+++ R/FeatureStore.R
@@ -48,8 +48,14 @@ FeatureStore <- R6::R6Class(
 }
 
 .craft_geo_features <- function(private){
-  geo_features <-
-    .craft_tidy_data(private) %>%
-    dplyr::select(building_id, dplyr::starts_with("geo_"))
+  error_median_ranking <- c(9, 28, 27, 8, 11, 26, 2, 3, 13, 19, 20, 1, 10, 21, 24, 15, 12, 23, 7, 22, 6, 16, 14, 5, 0, 30, 18, 25, 4, 17, 29)
+
+  .craft_tidy_data(private) %>%
+    dplyr::select(building_id, dplyr::starts_with("geo_")) %>%
+    dplyr::transmute(
+      building_id = building_id,
+      geo_level_1_id_ordered = ordered(geo_level_1_id, levels = error_median_ranking),
+      geo_level_1_id_integer = as.numeric(geo_level_1_id_ordered)
+    )
 }
 
--- tests/component-tests/test-FeatureStore.R
+++ tests/component-tests/test-FeatureStore.R
@@ -13,12 +13,14 @@ test_that("FeatureStore has access to the project datasets", {
     attach(test_env)
     expect_class(obj$tidy_data, "data.frame")
     expect_table_has_col_names(obj$tidy_data, c("source", "building_id"))
+    expect_has_no_duplicates(obj$tidy_data$building_id)
 })
 
 test_that("FeatureStore has access to the new feature sets", {
     attach(test_env)
+    geo_feature_names <- c("building_id", "geo_level_1_id_ordered", "geo_level_1_id_integer")
     expect_class(obj$geo_features, "data.frame")
-    expect_table_has_col_names(obj$geo_features, c("building_id"))
+    expect_table_has_col_names(obj$geo_features, c("building_id", geo_feature_names))
 })
 
 testthat::teardown(test_env <- NULL)
