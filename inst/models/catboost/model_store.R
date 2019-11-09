aa0e83740eca72eb33d1f728afba4872b115fd7c
harell <harel@loostiger.com>
2019-11-09 16:00:40 +1300

    updated component

--- inst/models/catboost/model_store.R
+++ inst/models/catboost/model_store.R
@@ -2,9 +2,9 @@
 #' @description Log the results (predictions and model objects) in a concise and
 #'   consistent manner.
 model_store <- function(){
-    target <- file.path(getOption("path_archive", default = tempdir()), model_name)
+    ## Store the files generated during training
+    target <- file.path(getOption("path_archive", default = tempdir()), model_name, "logs")
     source <- params$train_dir
-
     dir.create(target, showWarnings = FALSE, recursive = TRUE)
     file.copy(source, target, recursive = TRUE)
 
