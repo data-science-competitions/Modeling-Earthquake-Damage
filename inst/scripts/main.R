6b3f53baae1059045a0481dc4c443a8fcc710408
harell <harel@loostiger.com>
2019-11-09 16:55:33 +1300

    implemented #12

--- inst/scripts/main.R
+++ inst/scripts/main.R
@@ -12,7 +12,7 @@ set.seed(1936)
 rset_obj <- historical_data %>% rsample::initial_split(prop = 0.8, strata = "damage_grade")
 role_pk <- "building_id"
 role_none <- NULL
-role_input <- match_columns(historical_data, "^geo_|^has_")
+role_input <- match_columns(historical_data, "^geo_|^has_superstructure_|^has_secondary_use$")
 role_target <- "damage_grade"
 
 train_set <-
