5d63c73fa04dc3762435eef02e89b90e456dc470
harell <harel@loostiger.com>
2019-11-11 20:49:43 +1300

    randomForest model

---
+++ inst/models/randomForest/model_store.R
@@ -0,0 +1,6 @@
+#' @title Log the Results
+#' @description Log the results (predictions and model objects) in a concise and
+#'   consistent manner.
+model_store <- function(){
+    return(invisible())
+}
