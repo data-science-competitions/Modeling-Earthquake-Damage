aaf58d4e8f90341c16f13f0125541edf37a14b32
harell <harel@loostiger.com>
2019-11-17 13:24:28 +1300

    run testthat in local environment

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,13 @@
-GlobalEnv_funs <- as.vector(lsf.str(envir = .GlobalEnv))
-source(list.files(pattern = "testthat-helpers.R$", recursive = TRUE, full.names = TRUE))
-.setup()
+local({
+    GlobalEnv_funs <- as.vector(lsf.str(envir = .GlobalEnv))
+    source(list.files(pattern = "testthat-helpers.R$", recursive = TRUE, full.names = TRUE))
+    .setup()
 
-.run_unit_tests()
-.run_component_tests()
-.run_integration_tests()
-.run_coverage_tests()
+    .run_unit_tests()
+    .run_component_tests()
+    .run_integration_tests()
+    .run_coverage_tests()
 
-.cleanup()
-rm(GlobalEnv_funs, list = setdiff(as.vector(lsf.str(envir = .GlobalEnv)), GlobalEnv_funs))
+    .cleanup()
+    rm(GlobalEnv_funs, list = setdiff(as.vector(lsf.str(envir = .GlobalEnv)), GlobalEnv_funs))
+}, envir = testthat::test_env())
