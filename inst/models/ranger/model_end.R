8042db130083401304443566bfa938e8d7b6fd31
harell <harel@loostiger.com>
2019-11-17 18:40:29 +1300

    Partial Dependence Plots for ranger

--- inst/models/ranger/model_end.R
+++ inst/models/ranger/model_end.R
@@ -15,9 +15,24 @@ model_end <- function(){
     )
 
     ## Visualisation: Agnostic Variable Importance
-    figure_path <- file.path(target, "variable-importance-agnostic.jpg")
-    explanation <- ingredients::feature_importance(explainer, type = "difference") %>% plot()
-    ggplot2::ggsave(figure_path, explanation, "jpeg", width = 297, height = 210, units = "mm")
+    figure_path <- file.path(target, "(variable-importance)(agnostic).jpg")
+    fi <- ingredients::feature_importance(explainer, type = "difference")
+    ggplot2::ggsave(figure_path, fi %>% plot(), "jpeg", width = 297, height = 210, units = "mm")
+
+    ## Visualisation: Partial Dependence Plots
+    var_names <-
+        fi %>%
+        dplyr::arrange(dplyr::desc(dropout_loss)) %>%
+        dplyr::filter(!variable %in% c("_baseline_", "_full_model_")) %>%
+        top_n(10) %>% .$variable %>% as.character()
+    for(var_name in var_names){
+        n_values <- dplyr::n_distinct(explainer$data[, var_name])
+        if(n_values <= 2 | n_values > 32) next
+        variable_type <- dplyr::if_else("factor" %in% class(explainer$data[, var_name]), "categorical", "numerical")
+        figure_path <- file.path(target, paste0("(pdp)(", var_name, ").jpg"))
+        pdp <- ingredients::partial_dependency(explainer, var_name, variable_type = variable_type)
+        ggplot2::ggsave(figure_path, pdp %>% plot(), "jpeg", width = 297, height = 210, units = "mm")
+    }
 
     return(invisible())
 }
