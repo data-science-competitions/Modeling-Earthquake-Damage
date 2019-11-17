657c835dd20afc5656ec0829dc93525a31b55a2f
harell <harel@loostiger.com>
2019-11-17 19:04:15 +1300

    added PDP to all models

--- inst/models/ranger/model_end.R
+++ inst/models/ranger/model_end.R
@@ -4,6 +4,7 @@ model_end <- function(){
     ## Explore, Explain, and Debug model
     target <- file.path(getOption("path_archive", default = tempdir()), model_name, "explanations")
     dir.create(target, showWarnings = FALSE, recursive = TRUE)
+
     explainer <- DALEX::explain(
         model = model_object,
         data = historical_data[, role_input],
@@ -13,6 +14,9 @@ model_end <- function(){
         label = model_name,
         verbose = getOption("verbose")
     )
+
+    ## Report: Model Performance
+    print(ExplainerYardstick(explainer)$delete_label(".estimator")$insert_label(".dataset", "historical_data")$all_numeric_metrics)
 
     ## Visualisation: Agnostic Variable Importance
     figure_path <- file.path(target, "(variable-importance)(agnostic).jpg")
@@ -24,12 +28,13 @@ model_end <- function(){
         fi %>%
         dplyr::arrange(dplyr::desc(dropout_loss)) %>%
         dplyr::filter(!variable %in% c("_baseline_", "_full_model_")) %>%
-        top_n(10) %>% .$variable %>% as.character()
+        slice(1:5) %>% .$variable %>% as.character()
     for(var_name in var_names){
-        n_values <- dplyr::n_distinct(explainer$data[, var_name])
-        if(n_values <= 2 | n_values > 32) next
-        variable_type <- dplyr::if_else("factor" %in% class(explainer$data[, var_name]), "categorical", "numerical")
-        figure_path <- file.path(target, paste0("(pdp)(", var_name, ").jpg"))
+        var_class <- class(explainer$data[, var_name])
+        var_levels <- dplyr::n_distinct(explainer$data[, var_name])
+        variable_type <- dplyr::if_else("factor" %in% var_class, "categorical", "numerical")
+        if(var_levels <= 2 | ("factor" %in% var_class & var_levels > 32)) next
+        figure_path <- file.path(target, paste0("(pdp)(", which(var_names %in% var_name),")(",var_name, ").jpg"))
         pdp <- ingredients::partial_dependency(explainer, var_name, variable_type = variable_type)
         ggplot2::ggsave(figure_path, pdp %>% plot(), "jpeg", width = 297, height = 210, units = "mm")
     }
--- inst/models/xgboost/model_end.R
+++ inst/models/xgboost/model_end.R
@@ -14,12 +14,30 @@ model_end <- function(){
         label = model_name,
         verbose = getOption("verbose")
     )
+
+    ## Report: Model Performance
     print(ExplainerYardstick(explainer)$delete_label(".estimator")$insert_label(".dataset", "historical_data")$all_numeric_metrics)
 
     ## Visualisation: Agnostic Variable Importance
-    figure_path <- file.path(target, "variable-importance-agnostic.jpg")
-    explanation <- ingredients::feature_importance(explainer, type = "difference") %>% plot()
-    ggplot2::ggsave(figure_path, explanation, "jpeg", width = 297, height = 210, units = "mm")
+    figure_path <- file.path(target, "(variable-importance)(agnostic).jpg")
+    fi <- ingredients::feature_importance(explainer, type = "difference")
+    ggplot2::ggsave(figure_path, fi %>% plot(), "jpeg", width = 297, height = 210, units = "mm")
+
+    ## Visualisation: Partial Dependence Plots
+    var_names <-
+        fi %>%
+        dplyr::arrange(dplyr::desc(dropout_loss)) %>%
+        dplyr::filter(!variable %in% c("_baseline_", "_full_model_")) %>%
+        slice(1:5) %>% .$variable %>% as.character()
+    for(var_name in var_names){
+        var_class <- class(explainer$data[, var_name])
+        var_levels <- dplyr::n_distinct(explainer$data[, var_name])
+        variable_type <- dplyr::if_else("factor" %in% var_class, "categorical", "numerical")
+        if(var_levels <= 2 | ("factor" %in% var_class & var_levels > 32)) next
+        figure_path <- file.path(target, paste0("(pdp)(", which(var_names %in% var_name),")(",var_name, ").jpg"))
+        pdp <- ingredients::partial_dependency(explainer, var_name, variable_type = variable_type)
+        ggplot2::ggsave(figure_path, pdp %>% plot(), "jpeg", width = 297, height = 210, units = "mm")
+    }
 
     ## Visualisation: XGBoost Variable Importance
     figure_path <- file.path(target, "variable-importance-xgboost.jpg")
