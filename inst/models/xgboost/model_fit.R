0666d4bebb7c8203b33575ce7f51bc17fa77871e
harell <harel@loostiger.com>
2019-11-25 15:16:50 +1300

    changed objective function to log cos hyperbolic

--- inst/models/xgboost/model_fit.R
+++ inst/models/xgboost/model_fit.R
@@ -16,6 +16,7 @@ model_fit <- function(historical_data, model_formula)
         data = xgb_train,
         nrounds = params$nrounds,
         watchlist = list(train = xgb_train, test = xgb_test),
+        obj = log_cosh_obj,
         feval = feval_f1,
         print_every_n = 10,
         early_stopping_rounds = 50,
