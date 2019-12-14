7c05eb776483b6ec3049e833d1601b67f74308dc
harell <harel@loostiger.com>
2019-12-14 21:25:20 +1300

    Run Learners

---
+++ inst/scripts/main-ensemble.R
@@ -0,0 +1,94 @@
+# Configuration -----------------------------------------------------------
+options(verbose = FALSE)
+enable_parallelism()
+
+# Setup -------------------------------------------------------------------
+emsemble <- new.env()
+fs <- FeatureStore$new()
+model_names <- c(
+    "arithmetic-mean", # [1]
+    "rpart",           # [2]
+    "ranger",          # [3]
+    "catboost",        # [4]
+    "randomForest",    # [5]
+    "xgboost"          # [6]
+)[c(1,6)]
+output_dir <- file.path(getOption("path_archive"), "stacked-models")
+dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
+
+# Get the Data ------------------------------------------------------------
+tidy_data <-
+    fs$tidy_data #%>%
+# dplyr::left_join(by = "building_id", fs$geo_features) %>%
+# dplyr::left_join(by = "building_id", fs$mfa_features) %>%
+# dplyr::left_join(by = "building_id", fs$age_features)
+
+# Features Selection ------------------------------------------------------
+role_pk <- "building_id"
+role_none <- "geo_level_3_id"
+role_input_1 <- match_columns(tidy_data, "_type$")
+role_input_2 <- match_columns(tidy_data, "^has_superstructure_mud_mortar_stone$")
+role_input_3 <- match_columns(tidy_data, "^geo_level_[1-3]_id_[cat]|^mfa_dim_")
+role_input_4 <- match_columns(tidy_data, "^age$|_percentage$|^count_")
+role_input <- unique(c(role_input_1, role_input_2, role_input_3, role_input_4))
+role_target <- "damage_grade"
+
+# Sample the Data ---------------------------------------------------------
+set.seed(1715)
+rset_obj <-
+    tidy_data %>%
+    dplyr::filter(.set_source %in% "historical_data") %>%
+    dplyr::sample_n(1e5) %>%
+    rsample::group_vfold_cv(group = "geo_level_3_id", v = 2)
+
+# Run Learners ------------------------------------------------------------
+iterators <- expand.grid(model_name = model_names, split = 1:get_rsample_num_of_splits(rset_obj), stringsAsFactors = FALSE)
+responses <- list()
+for(i in seq_len(nrow(iterators))){
+    model_name <- iterators[i, "model_name"]
+    split <- iterators[i, "split"]
+    message("| model_name: ", model_name, " | split: ", split, " |")
+
+    train_set <-
+        get_rsample_training_set(rset_obj, split) %>%
+        dplyr::select(-dplyr::starts_with(".")) %>%
+        dplyr::select(role_pk, role_input, role_target, role_none)
+
+    test_set <-
+        get_rsample_test_set(rset_obj, split) %>%
+        dplyr::select(-dplyr::starts_with(".")) %>%
+        dplyr::select(role_pk, role_input, role_target, role_none)
+
+    pm <- PentaModel$new(path = file.path(.Options$path_models, model_name))
+    pm$set_historical_data(train_set)
+    pm$set_new_data(test_set)
+    pm$set_role_pk(role_pk)
+    pm$set_role_input(role_input)
+    pm$set_role_target(role_target)
+
+    pm$model_init()
+    pm$model_fit()
+    pm$model_predict()
+
+    model_response <- pm$response %>% dplyr::select(role_pk, fit) %>% dplyr::rename(!!model_name := fit)
+    responses <- append(responses, list(model_response))
+}
+level_1 <- responses %>% dplyr::bind_rows() %>% group_by_at(role_pk) %>% dplyr::summarise_all(sum, na.rm = TRUE)
+
+# Run Super Learner -------------------------------------------------------
+
+
+
+# Evaluate Model ----------------------------------------------------------
+# data <-
+#     test_set %>%
+#     dplyr::left_join(response, by = role_pk) %>%
+#     dplyr::rename("truth.numeric" = !!role_target, "estimate.numeric" = "fit") %>%
+#     dplyr::mutate(truth.class = as_earthquake_damage(truth.numeric), estimate.class = as_earthquake_damage(estimate.numeric))
+
+## Ungrouped Evaluation (overview)
+# Yardstick$
+#     new(data %>% dplyr::ungroup(), truth = "truth.class", estimate = "estimate.class")$
+#     set_estimator("micro")$
+#     insert_label(".model", pm$model_name)$
+#     f_meas
