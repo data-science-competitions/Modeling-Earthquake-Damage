00ed9d798f56121a9fd25cddf96e85e01f44158e
harell <harel@loostiger.com>
2019-12-14 13:18:14 +1300

    closeAllConnections

--- R/utils-parallel.R
+++ R/utils-parallel.R
@@ -8,11 +8,13 @@
 #'
 enable_parallelism <- function() {#nocov
     if(is.null(getOption("parallel.cluster"))){
+        closeAllConnections()
         options(parallel.enable = TRUE)
         options(parallel.ncores = parallel::detectCores())
-        options(parallel.cluster = parallel::makeCluster(getOption("parallel.ncores")))
+        options(parallel.cluster = parallel::makePSOCKcluster(getOption("parallel.ncores")))
         parallel::setDefaultCluster(cl = getOption("parallel.cluster"))
     }
+    message("\033[42msocket cluster with ", getOption("parallel.ncores"), " workers is online\033[49m")
     invisible()
 }
 #nocov end
