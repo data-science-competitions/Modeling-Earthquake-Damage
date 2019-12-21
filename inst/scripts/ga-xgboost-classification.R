f4193215e9182cb853c62200193c487fc3a5f611
harell <harel@loostiger.com>
2019-12-21 14:53:29 +1300

    ga-xgboost-classification

--- inst/scripts/ga-xgboost-classification.R
+++ inst/scripts/ga-xgboost-classification.R
@@ -1,24 +1,31 @@
+# Configuration -----------------------------------------------------------
+options(verbose = FALSE)
+enable_parallelism()
+
 # Setup -------------------------------------------------------------------
 library(GA)
 fs <- FeatureStore$new()
-model_name <- "xgboost"
+model_name <- "xgboost-classification"
 output_dir <- file.path(getOption("path_archive"), model_name)
 dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
 
 # Get the Data ------------------------------------------------------------
-tidy_data <- fs$tidy_data %>% dplyr::left_join(by = "building_id", fs$geo_features)
+tidy_data <-
+    fs$tidy_data %>%
+    dplyr::left_join(by = "building_id", fs$geo_features) %>%
+    dplyr::left_join(by = "building_id", fs$mfa_features) %>%
+    dplyr::left_join(by = "building_id", fs$age_features)
+
+# Get Variables Names -----------------------------------------------------
+yaml_path <- file.path(getOption("path_models"), "model-variables.yml")
+yaml_list <- yaml::read_yaml(yaml_path, eval.expr = TRUE)
+list2env(yaml_list$default %>% lapply(merge_elements), envir = globalenv())
+
+# Sample the Data ---------------------------------------------------------
 historical_data <- tidy_data %>% dplyr::filter(.set_source %in% "historical_data")
 new_data <- tidy_data %>% dplyr::filter(.set_source %in% "new_data")
 
-# Sample the Data ---------------------------------------------------------
-role_pk <- "building_id"
-role_none <- NULL
-role_input_1 <- match_columns(historical_data, "^geo_level_")
-role_input_2 <- match_columns(historical_data, "^age")
-role_input_3 <- match_columns(historical_data, "^has_superstructure_mud_mortar_stone$|_type$")
-role_input <- c(role_input_1, role_input_2, role_input_3)
-role_target <- "damage_grade"
-
+# Split the Data ----------------------------------------------------------
 train_set <-
     historical_data %>%
     dplyr::filter(.set_role %in% "train") %>%
@@ -63,30 +70,25 @@ eval_function <- function(params_values){ #browser()
         test_set %>%
         dplyr::left_join(pm$response, by = role_pk) %>%
         dplyr::rename("truth.numeric" = !!role_target, "estimate.numeric" = "fit") %>%
-        dplyr::mutate(truth.class = as_earthquake_damage(truth.numeric), estimate.class = as_earthquake_damage(estimate.numeric)) %>%
-        dplyr::group_by(geo_level_1_id)
+        dplyr::mutate(truth.class = as_earthquake_damage(truth.numeric), estimate.class = as_earthquake_damage(estimate.numeric))
 
-    accuracy <- yardstick::accuracy_vec(data$truth.class, data$estimate.class)
-    mae <- yardstick::mae_vec(data$truth.numeric, data$estimate.numeric)
-    rmse <- yardstick::rmse_vec(data$truth.numeric, data$estimate.numeric)
-    print(paste("accuracy =", round(accuracy,3), "| mae =", round(mae,3), "| rmse =", round(rmse,3)))
-
-    return(rmse)
+    f1_micro <- yardstick::f_meas(data, truth = "truth.class", estimate = "estimate.class", estimator = "micro")
+    return(f1_micro[[".estimate"]])
 }
 
 # Run Genetic Algorithm ---------------------------------------------------
 ga_obj <- GA::ga(
     type = "real-valued",
     fitness = eval_function,
-    lower = c(0.001, 0.01, 0.01, 0.01), # minimum values
-    upper = c(0.999, 20.0, 9.99, 9.99), # maximum values
-    names = c("eta", "gamma", "lambda", "alpha"),
+    lower = c(0.01, 01, 01, 0.01, 0.01), # minimum values
+    upper = c(0.10, 20, 50, 0.99, 0.99), # maximum values
+    names = c("eta", "max_depth", "min_child_weight", "subsample", "colsample_bytree"),
     popSize = 2^4,    # population size
     maxiter = 2^3,    # number of iterations
     pcrossover = 0.8, # probability of crossover between pairs of chromosomes
     pmutation = 0.5,  # probability of mutation
     elitism = 0.25,   # percentage of elitism (fraction of best current solutions used on next round)
-    suggestions = c(0.13, 1.09, 1.98, 1.64),
+    suggestions = c(0.02, 06, 15, 0.95, 0.80),
     parallel = FALSE,
     optim = FALSE,
     keepBest = TRUE,
